2d9e660064f2105fc187e4b0b5aa59fd0d38556a
Yasin <ilikebreadtoomuch@gmail.com>
2025-04-16 13:14:31 +0300

    conn: revert 'use only one connection on plan9'
    misleading commit message and did not work anyway

--- conn/bind_std.go
+++ conn/bind_std.go
@@ -160,17 +160,14 @@ again:
 	var v4pc *ipv4.PacketConn
 	var v6pc *ipv6.PacketConn
 
-	if runtime.GOOS == "plan9" {
-		v6conn, port, err = listenNet("udp", port)
-		if err != nil && !errors.Is(err, syscall.EAFNOSUPPORT) {
-			return nil, 0, err
-		}
-	} else {
-		v4conn, port, err = listenNet("udp4", port)
-		if err != nil && !errors.Is(err, syscall.EAFNOSUPPORT) {
-			return nil, 0, err
-		}
+	v4conn, port, err = listenNet("udp4", port)
+	if err != nil && !errors.Is(err, syscall.EAFNOSUPPORT) {
+		return nil, 0, err
+	}
 
+	if runtime.GOOS == "plan9" {
+		v6conn, port = v4conn, port
+	} else {
 		// Listen on the same port as we're using for ipv4.
 		v6conn, port, err = listenNet("udp6", port)
 		if uport == 0 && errors.Is(err, errEADDRINUSE) && tries < 100 {
@@ -183,7 +180,6 @@ again:
 			return nil, 0, err
 		}
 	}
-
 	var fns []ReceiveFunc
 	if v4conn != nil {
 		s.ipv4TxOffload, s.ipv4RxOffload = supportsUDPOffload(v4conn)
@@ -361,7 +357,7 @@ func (s *StdNetBind) Send(bufs [][]byte, endpoint Endpoint) error {
 	offload := s.ipv4TxOffload
 	br := batchWriter(s.ipv4PC)
 	is6 := false
-	if runtime.GOOS == "plan9" || endpoint.DstIP().Is6() {
+	if endpoint.DstIP().Is6() {
 		blackhole = s.blackhole6
 		conn = s.ipv6
 		br = s.ipv6PC
